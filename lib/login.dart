55e8b3b3310b858e05588ccc6efa109557149e71
devjs2705 <devjs2705@gmail.com>
2024-01-24 21:36:07 +0530

    login

--- lib/login.dart
+++ lib/login.dart
@@ -37,7 +37,7 @@ class _MyLoginState extends State<MyLogin> {
               child: Column(
                 children: [
                   Container(
-                    height: 240,
+                    height: 250,
                     width: 300,
                     decoration: BoxDecoration(
                         color: Colors.transparent,
