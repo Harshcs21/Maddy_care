bb9b84eac3cdd77ca0a5bc67109277158a2a549b
Harsh21cs <harsh.cs21@bmsce.ac.in>
2024-01-24 21:45:16 +0530

    doctorinfo page done

--- lib/doctorinfo.dart
+++ lib/doctorinfo.dart
@@ -1,3 +1,5 @@
+import 'dart:js_util';
+
 import 'package:flutter/material.dart';
 
 class MyDoctor extends StatefulWidget {
@@ -8,31 +10,87 @@ class MyDoctor extends StatefulWidget {
 }
 
 class _MyDoctorState extends State<MyDoctor> {
+
+final List names = [
+  'Dr. Dev',
+  'Dr. Harsh',
+  'Dr. Himanshu',
+  'Dr. Ayushmaan'
+];
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      backgroundColor: Colors.blue,
+    return Scaffold(
         body: SafeArea(
-            child: Center(
-              child: Padding(
-                  padding: EdgeInsets.all(16),
-                  child: Column(
-                    // mainAxisAlignment: MainAxisAlignment.center,
-                    // crossAxisAlignment: CrossAxisAlignment.stretch,
-                    children: [
-                      Column(
+            child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16),
+                child: Column(
+                  // mainAxisAlignment: MainAxisAlignment.center,
+                  // crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Container(
+                      margin: EdgeInsets.symmetric(vertical: 20),
+                      padding: EdgeInsets.all(10),
+                      width: double.infinity,
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
                         children: [
-                          Text('data',style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 100
-                          ),)
+                          Column(
+                            children: [
+                              Text("Dr. Shaliendra"),
+                              Text("Total Appointments : 23"),
+                              Text("Online Appointments : 30"),
+                              Text("Offline Appointments : 20"),
+                            ],
+                          ),
+                          Container(
+                            height: 100,
+                            width: 100,
+                            decoration: BoxDecoration(
+                              image: DecorationImage(
+                                image: AssetImage('assets/login_page.png'),
+                              )
+                            ),
+                          )
                         ],
-                      )
-                    ],
-                  )
-              ),
-              
+                      ),
+                    ),
 
+                    Container(
+                      width: double.infinity,
+                      alignment: Alignment.topLeft,
+                      child: Text(
+                          "Upcomming Appointments",
+                        style: TextStyle(
+                          fontSize: 18,
+                        ),
+                      ),
+                    ),
+
+                    SizedBox(height: 10),
+
+                    Expanded(
+                        child: ListView.builder(
+                            itemCount: names.length,
+                            itemBuilder: (context, index)
+                        {
+                            return Container(
+                              height: 20,
+                              width: 20,
+                              color: Colors.black,
+                            );
+                        }
+                        )
+                    )
+                    
+                    
+
+                  ],
+                )
             )
         ),
     );
