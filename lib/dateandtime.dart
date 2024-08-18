1551b66bf5f58be40badbb825f8791494080ece2
devjs2705 <devjs2705@gmail.com>
2024-08-19 00:48:03 +0530

    checkout page, pages reshifted, date and Time page updated, other changes done

--- lib/dateandtime.dart
+++ lib/dateandtime.dart
@@ -1,28 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_pro/doctordetail.dart';
+import 'checkout.dart';
 import 'patientdetails.dart';
 
 class DateAndTime extends StatefulWidget {
 
-  String? doctor_name;
-  String? doctor_uid;
-  String? doctor_image;
+  final String doctorName;
+  final String doctorUid;
+  final String doctorSpec;
+  final String patientName;
+  final String patientAge;
+  final String patientMobile;
 
-  DateAndTime({Key? key, required this.doctor_name, required this.doctor_uid, required this.doctor_image}) : super(key: key);
+  const DateAndTime({super.key, required this.doctorName, required this.doctorUid, required this.patientName, required this.patientAge, required this.patientMobile, required this.doctorSpec,});
 
   @override
-  State<DateAndTime> createState() => _DateAndTimeState(doctor_name: doctor_name, doctor_image: doctor_image, doctor_uid: doctor_uid);
+  State<DateAndTime> createState() => _DateAndTimeState();
 }
 
 class _DateAndTimeState extends State<DateAndTime> {
 
-  String? doctor_name;
-  String? doctor_uid;
-  String? doctor_image;
-
-  _DateAndTimeState({required this.doctor_name, required this.doctor_uid, required this.doctor_image});
-
-
+  bool isClicked = false;
+  String shift = "Morning";
 
   DateTime _selectedDate = DateTime.now();
   TimeOfDay _selectedTime = TimeOfDay.now();
@@ -53,20 +52,20 @@ class _DateAndTimeState extends State<DateAndTime> {
     }
   }
 
-  void _navigateToDoctorDetails(BuildContext context, String? doctor_name, String? doctor_image, String? doctor_uid) {
-    Navigator.push(
-      context,
-      MaterialPageRoute(
-        builder: (context) => DoctorDetails(doctor_name: doctor_name, doctor_image: doctor_image, doctor_uid: doctor_uid),
-      ),
-    );
-  }
+  // void _navigateToDoctorDetails(BuildContext context, String? doctor_name, String? doctor_image, String? doctor_uid) {
+  //   Navigator.push(
+  //     context,
+  //     MaterialPageRoute(
+  //       builder: (context) => DoctorDetails(doctor_name: doctor_name, doctor_image: doctor_image, doctor_uid: doctor_uid),
+  //     ),
+  //   );
+  // }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('    Select Date and Time'),
+        title: Text('Select Date and Shift'),
       ),
       body: Center(
         child: Column(
@@ -98,28 +97,75 @@ class _DateAndTimeState extends State<DateAndTime> {
             ),
             SizedBox(height: 40),
             Text(
-              'Select Time',
+              'Select Shift',
               style: TextStyle(
                 fontSize: 24,
                 fontWeight: FontWeight.bold,
               ),
             ),
-            SizedBox(height: 10),
-            Text(
-              '${_selectedTime.hour}:${_selectedTime.minute}',
-              style: TextStyle(
-                fontSize: 20,
-              ),
-            ),
-            SizedBox(height: 10),
-            ElevatedButton(
-              onPressed: () => _selectTime(context),
-              child: Text('Select Time',
-              style: TextStyle(color: Colors.white,)
-              ),
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.green,
-              ),
+            SizedBox(height: 20),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children : [
+                Column(
+                  children: [
+                    InkWell(
+                      onTap: ()=>{
+                        setState(() {
+                          if(isClicked == true)
+                            isClicked = false;
+                            shift = 'Morning';
+                        })
+                      },
+                      child: Container(
+                        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                        decoration: BoxDecoration(
+                          color: !isClicked ?Colors.green :Colors.greenAccent,
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        child: Text("9-12", style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                        ),)
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text("Morning", style: TextStyle(
+                      fontSize: 20
+                    ))
+                  ]
+                ),
+                Column(
+                    children: [
+                      InkWell(
+                        onTap: ()=>{
+                          setState(() {
+                            if(isClicked == false)
+                              {
+                                isClicked = true;
+                                shift = "Evening";
+                              }
+                          })
+                        },
+                        child: Container(
+                            padding: EdgeInsets.symmetric(horizontal: 30, vertical: 10),
+                            decoration: BoxDecoration(
+                              color: isClicked ?Colors.green :Colors.greenAccent,
+                              borderRadius: BorderRadius.circular(10.0),
+                            ),
+                            child: Text("3 - 6", style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20,
+                            ),)
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                      Text("Evening", style: TextStyle(
+                          fontSize: 20
+                      ))
+                    ]
+                ),
+              ]
             ),
             Spacer(), // Add spacer to push the continue button to the bottom
             ElevatedButton(
@@ -127,7 +173,8 @@ class _DateAndTimeState extends State<DateAndTime> {
                 date = _selectedDate.toString();
                 time = _selectedTime.toString();
 
-                 _navigateToDoctorDetails(context, doctor_name, doctor_image, doctor_uid);
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Checkout(date: date, shift: shift, doctorSpec: widget.doctorSpec, doctorName: widget.doctorName, doctorUid: widget.doctorUid,  patientName: widget.patientName, patientAge: widget.patientAge, patientMobile: widget.patientMobile)));
+                 // _navigateToDoctorDetails(context, doctor_name, doctor_image, doctor_uid);
               },
               style: ElevatedButton.styleFrom(
                 backgroundColor: Colors.green,
--- lib/firebase_auth_service.dart
+++ lib/firebase_auth_service.dart
@@ -1,12 +1,8 @@
-
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_pro/reg_doc_user.dart';
 import 'register.dart';
 import 'package:uuid/uuid.dart';
-
-
-
 
 class AuthMethods {
   final FirebaseAuth _auth = FirebaseAuth.instance;
@@ -22,58 +18,66 @@ class AuthMethods {
   }) async {
     String res = "Some error occured";
 
-    if(global == 'users')
-      {
-        try {
-          if (email.isNotEmpty || username.isNotEmpty || password.isNotEmpty || confirmPassword.isNotEmpty) {
-            // register user
-            UserCredential cred = await _auth.createUserWithEmailAndPassword(
-                email: email, password: password);
-            print(cred.user!.uid);
+    if (global == 'users') {
+      try {
+        if (email.isNotEmpty ||
+            username.isNotEmpty ||
+            password.isNotEmpty ||
+            confirmPassword.isNotEmpty) {
+          // register user
+          UserCredential cred = await _auth.createUserWithEmailAndPassword(
+              email: email, password: password);
+          print(cred.user!.uid);
 
-            // add user to our databse
-            _firestore.collection(global).doc(cred.user!.uid).set({
-              'username': username,
-              'uid': cred.user!.uid,
-              'email': email,
-              'password': password,
-              'confirmPassword': confirmPassword
-            });
-            res = "Success";
-          }
-        } catch (err) {
-          res = err.toString();
+          // add user to our databse
+          _firestore.collection(global).doc(cred.user!.uid).set({
+            'username': username,
+            'uid': cred.user!.uid,
+            'email': email,
+            'password': password,
+            'confirmPassword': confirmPassword
+          });
+          res = "Success";
         }
-
-        return res;
+      } catch (err) {
+        res = err.toString();
       }
-      else
-        {
-          try {
-            if (email.isNotEmpty || username.isNotEmpty || password.isNotEmpty || confirmPassword.isNotEmpty) {
-              // register user
-              UserCredential cred = await _auth.createUserWithEmailAndPassword(
-                  email: email, password: password);
-              print(cred.user!.uid);
 
-              // add user to our databse
-              _firestore.collection(global).doc('specializations').collection(specialization).doc(cred.user!.uid).set({
-                'username': username,
-                'uid': cred.user!.uid,
-                'email': email,
-                'password': password,
-                'confirmPassword': confirmPassword
-              });
-              res = "Success";
-            }
-          } catch (err) {
-            res = err.toString();
-          }
+      return res;
+    } else {
+      try {
+        if (email.isNotEmpty ||
+            username.isNotEmpty ||
+            password.isNotEmpty ||
+            confirmPassword.isNotEmpty) {
+          // register user
+          UserCredential cred = await _auth.createUserWithEmailAndPassword(
+              email: email, password: password);
+          print(cred.user!.uid);
 
-          return res;
+          // add user to our databse
+          _firestore
+              .collection(global)
+              .doc('specializations')
+              .collection(specialization)
+              .doc(cred.user!.uid)
+              .set({
+            'username': username,
+            'uid': cred.user!.uid,
+            'email': email,
+            'password': password,
+            'confirmPassword': confirmPassword
+          });
+          res = "Success";
         }
+      } catch (err) {
+        res = err.toString();
+      }
 
+      return res;
+    }
   }
+
   // Future <String> loginUser({
   //   required String email,
   //   required String password,
@@ -110,14 +114,36 @@ class AuthMethods {
         );
 
         // Check if the user is a doctor
-        DocumentSnapshot doctorSnapshot = await _firestore.collection('Doctors').doc(userCredential.user!.uid).get();
-        if (doctorSnapshot.exists) {
-          // Navigate to doctor's page
-          result = "Doctor";
-        } else {
-          // Navigate to patient's page
-          result = "user";
+
+        List<String> specs = ["ENT", "Physician", "Cardiologist", "Dentist"];
+        // late DocumentSnapshot doctorSnapshot;
+        for (String specialize in specs) {
+          print(specialize);
+          DocumentSnapshot doctorSnapshot = await _firestore
+              .collection('Doctors')
+              .doc('specializations')
+              .collection(specialize)
+              .doc(userCredential.user!.uid)
+              .get();
+
+          if(doctorSnapshot.exists)
+            result = "Doctor";
+          break;
         }
+
+        if(!(result == "Doctor"))
+          {
+            result = "User";
+          }
+
+        // // DocumentSnapshot doctorSnapshot = await _firestore.collection('Doctors').doc(userCredential.user!.uid).get();
+        // if (doctorSnapshot.exists) {
+        //   // Navigate to doctor's page
+        //   result = "Doctor";
+        // } else {
+        //   // Navigate to patient's page
+        //   result = "user";
+        // }
       } else {
         result = "Please enter all the fields";
       }
@@ -129,63 +155,59 @@ class AuthMethods {
   }
 
   Future<String> addAppointment({
-  required String name,
-  required String date,
-  required String time,
-  required String doctor,
-  required String phone,
-  required String doctor_uid,
-
-}) async{
-
+    required String name,
+    required String date,
+    required String time,
+    required String doctor,
+    required String phone,
+    required String doctor_uid,
+  }) async {
     String result = "Some error occured";
 
     try {
-      if(name.isNotEmpty && phone.isNotEmpty && doctor.isNotEmpty && date.isNotEmpty && time.isNotEmpty && doctor_uid.isNotEmpty)
-        {
-          var uuid = Uuid().v4();
-          var uuid2 = Uuid().v4();
+      if (name.isNotEmpty &&
+          phone.isNotEmpty &&
+          doctor.isNotEmpty &&
+          date.isNotEmpty &&
+          time.isNotEmpty &&
+          doctor_uid.isNotEmpty) {
+        var uuid = Uuid().v4();
+        var uuid2 = Uuid().v4();
 
-          await _firestore
-              .collection('users')
-              .doc(_auth.currentUser!.uid)
-              .collection('myAppointments')
-              .doc(uuid)
-              .set({
-            'id': uuid,
-            'patient name': name,
-            'date': date,
-            'doctor': doctor,
-            'phone': phone,
-            'time': time,
-            'doc_uid': doctor_uid,
-            'doc_appointment_id': uuid2,
-          });
+        await _firestore
+            .collection('users')
+            .doc(_auth.currentUser!.uid)
+            .collection('myAppointments')
+            .doc(uuid)
+            .set({
+          'id': uuid,
+          'patient name': name,
+          'date': date,
+          'doctor': doctor,
+          'phone': phone,
+          'time': time,
+          'doc_uid': doctor_uid,
+          'doc_appointment_id': uuid2,
+        });
 
-          await _firestore
-              .collection('Doctors')
-              .doc(doctor_uid)
-              .collection('myAppointments')
-              .doc(uuid2)
-              .set({
-            'id': uuid2,
-            'patient name': name,
-            'date': date,
-            // 'doctor': doctor,
-            'phone': phone,
-            'time': time,
-            'pat_uid': _auth.currentUser!.uid,
-            'pat_appointment_id': uuid,
-          });
+        await _firestore
+            .collection('Doctors')
+            .doc(doctor_uid)
+            .collection('myAppointments')
+            .doc(uuid2)
+            .set({
+          'id': uuid2,
+          'patient name': name,
+          'date': date,
+          // 'doctor': doctor,
+          'phone': phone,
+          'time': time,
+          'pat_uid': _auth.currentUser!.uid,
+          'pat_appointment_id': uuid,
+        });
 
-
-
-          result = "Successfully appointment booked";
-        }
-      else
-        {
-
-        }
+        result = "Successfully appointment booked";
+      } else {}
     } catch (err) {
       result = err.toString();
     }
@@ -202,13 +224,3 @@ class AuthMethods {
   //   return data;
   // }
 }
-
-
-
-
-
-
-
-
-
-
